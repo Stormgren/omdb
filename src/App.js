c33a90a45a69fb9ffbf906bcdcfcfd5570647839
Slobodan Vukovic <sbnv93@gmail.com>
2020-01-27 06:34:33 +0100

    Started to implement working search function

--- src/App.js
+++ src/App.js
@@ -12,33 +12,51 @@ class App extends Component {
       year: '',
       genre: '',
       plot: '',
-      rezultat: ''
+      rezultat: 'Guardians of Galaxy'
     }
   }
   
   componentDidMount() {
- 
+    
     const KEY = '9caa56dd'
-  
-
-  fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${KEY}`)
+    let query = this.state.rezultat
+    
+    fetch(`http://www.omdbapi.com/?s=${query}&apikey=${KEY}`)
     .then(res => (res.json())).then((data) => { this.setState({title: data.Title, poster: data.Poster,genre: data.Genre, year: data.Year, plot: data.Plot}, console.log(data)) } )
   }
 
+  handleChange = (e)=>{
+      this.setState({
+        rezultat: e.target.value
+      }, () => {
+        console.log(this.state.rezultat)
+      }
+      )
+
+
+    }
 submitHandler=(e)=>{
-  e.preventDefautlt();
-  this.setState({
-    rezultat: e.target.value
-  })
-}
+  e.preventDefault();
+   this.setState({
+     rezultat: e.target.value
+    })
+    // const KEY = '9caa56dd'
+    // let query = this.state.rezultat
+    
+    // fetch(`http://www.omdbapi.com/?s=${query}&apikey=${KEY}`)
+    // .then(res => (res.json())).then((data) => { this.setState({title: data.Title, poster: data.Poster,genre: data.Genre, year: data.Year, plot: data.Plot}, console.log(data)) } )
+ 
+    console.log(this.state.rezultat)
+  }
 render(){
   return (
     <div className="App">
       <header className="App-header">
         <div>
-          <input type="text"/>
-          <input type="submit" onSubmit={this.submitHandler}/>
+          <input type="text" value={this.state.rezultat} onChange={this.handleChange}/>
+          <button type="button" onClick={this.submitHandler}>Search</button>
         </div>
+        {this.state.rezultat}
         <Movie 
         title={this.state.title} 
         poster={this.state.poster} 
